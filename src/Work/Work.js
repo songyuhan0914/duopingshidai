6ec9391793a75b92a6abb26c56d3e2db689688dd
宋雨涵 <1548804335@qq.com>
2016-10-05 11:07:53 +0800

    Update Work.js

--- src/Work/Work.js
+++ src/Work/Work.js
@@ -7,22 +7,22 @@ class Work extends React.Component {
          <div className="work-card">
            <img src="http://7xopqp.com1.z0.glb.clouddn.com/contain-none.jpg" />
            <div className="work-text">
-             <h3>作品一</h3>
-             <p>2016.5.6</p>
+             <h3>mongoose</h3>
+             <p>2016.9.29</p>
            </div>
          </div>
          <div className="work-card">
            <img src="http://7xopqp.com1.z0.glb.clouddn.com/contain4.jpg" />
            <div className="work-text">
-             <h3>作品一</h3>
-             <p>2016.5.6</p>
+             <h3>用户端增删改查</h3>
+             <p>2016.9.30</p>
            </div>
          </div>
          <div className="work-card">
            <img src="http://7xopqp.com1.z0.glb.clouddn.com/contain1.jpg" />
            <div className="work-text">
-             <h3>作品一</h3>
-             <p>2016.5.6</p>
+             <h3>Post model</h3>
+             <p>2016.10.3</p>
            </div>
          </div>
          <div className="work-card">
